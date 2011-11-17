f9af79d0fe64760c32ba90920ac7fa2492afc984
Mathias Maske <mathias@maske-online.net>
2011-11-17 19:19:33 +0100

    Initial Commit

---
+++ dijkstra_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DijkstraApp::Application.config.secret_token = 'f50799442feddf31cf60adf53860c82107659bc6001d36f423bd1ba767d773edc112bc26003786aba54989794cd224a0536fd13e196258bd4c4912598ab044fe'
